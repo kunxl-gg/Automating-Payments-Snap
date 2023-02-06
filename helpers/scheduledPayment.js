b2b54eacda10aa40aef0ce2e8929ea1bba91edfd
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-02-06 17:50:42 +0530

    checked the condition to execute cronJob

---
+++ helpers/scheduledPayment.js
@@ -0,0 +1,67 @@
+// finally write the js function to check if the date matches the given one
+function checkDateMatch(scheduledDate, currentDate) {
+    let month = currentDate.getMonth() + 1;
+    let date = currentDate.getDate();
+    const year = currentDate.getFullYear();
+
+    month = month < 10 ? `0${month}` : month;
+    date = date < 10 ? `0${date}` : date;
+
+    const finalDateExpression = `${year}-${month}-${date}`
+
+    const hours = new Date().getHours();
+    const minutes = new Date().getMinutes();
+
+    const finalTimeExpression = `${hours}:${minutes}`;
+
+
+    // check if the dates match;
+    if (scheduledDate == `${finalDateExpression}T${finalTimeExpression}`) {
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+
+
+
+// check if the transaction is scheduled or not
+async function isTransactionScheduled() {
+    state = await ethereum.request({
+        method: 'snap_manageState',
+        params: ['get']
+    })
+
+    const scheduledDate = state.dateToStore;
+    const currentDate = new Date();
+
+    const isScheduled = checkDateMatch(scheduledDate, currentDate);
+}
+
+
+// made a function to make the scheduled payment
+async function makeScheduledTransaction(givnenState){
+    
+    // get the account details
+    const accounts = await ethereum.request({method: "eth_requestAccounts"});
+    const account = accounts[0];
+
+    // set transaction details
+    const transactionDetail = {
+        to: givenState.addrssToStore,
+        from: accounts[0],
+        value: givenState.amountToStore,
+        data:
+          '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
+        chainId: '0x5',
+    }
+
+    // make trasaction to the given address 
+    await etherem.request({
+        method: 'eth_sendTransaction',
+        params: [transactionDetail]
+    })
+    
+}
