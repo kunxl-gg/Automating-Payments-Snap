113eb7caf268dbf37288b15e9503e1e332f1fc4e
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-02-07 18:20:06 +0530

    made recurring payment

--- helpers/recurringPayments.js
+++ helpers/recurringPayments.js
@@ -13,10 +13,38 @@ async function checkMonthEnd(){
     })
 
     if(state.executeRecurringTransaction == 'true'){
-        state.recurringTransactionsList.map(()=>{
-            const detail = {
-                
-            }
+        makeInstallment(state);
+    }
+}
+
+async function makeInstallment(givenState){
+
+    // making a list of transaction details 
+    const finalTransactionDetailList = [];
+
+    const accounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+    })
+
+    const account = accounts[0];
+
+    givenState.recurringTransactionsList.map( data => {
+        const detail = {
+            to: data.Receiver, // Required except during contract publications.
+            from: account, // must match user's active address.
+            value: data.Value,// Only required to send ether to the recipient from the initiating external account.
+            data:
+                '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
+            chainId: '0x5', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
+        };
+
+        finalTransactionDetailList.push(detail);
+    })
+
+    for(let index = 0; index < finalTransactionDetailList.length; index++){
+        await ethereum.request({
+            method: 'eth_sendTransaction',
+            params: [finalTransactionDetailList[index]]
         })
     }
 }
